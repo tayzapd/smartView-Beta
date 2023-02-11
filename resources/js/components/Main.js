f9297fc2a1f5884d290bec7ebfcc2d8073be5efa
tayzapd <waiyanmyo220@gmail.com>
2023-02-11 01:07:57 -0500

    By Wai Yan

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './layouts/Layout';
 import Home from './layouts/Home';
+import About from './layouts/About';
+import Login from './layouts/Auth/Login';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext,useState } from 'react';
+import Context from '../Context';
 function Main() {
+
+    const [auth,setAuth] = useState(false);
+    const [user,setUser] = useState([]);
     return (
-        <BrowserRouter>
-            <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index element={<Home />} />
-            </Route>
-            </Routes>
-        </BrowserRouter>
+        <Context.Provider value={{}}>
+            <BrowserRouter>
+                <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/login' element={<Login />} />
+                </Route>
+                </Routes>
+            </BrowserRouter>
+        </Context.Provider>
     );
 }
 
