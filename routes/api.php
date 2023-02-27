e12667bdfd961be89cd50e96a3b8e02d9e9a3cd0
Thein Thein Soe <theintheinsoe1218@gmail.com>
2023-02-27 16:46:50 +0630

    update api route

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,66 @@ Route::controller(AuthController::class)->group(function(){
     Route::post('/login',"login");
 });
 
+Route::prefix('admin')->group(function(){
+    
+    Route::prefix('shoptypes')->controller(ShopTypeController::class)->group(function() {
+        Route::get('show','show');
+        Route::post('create','create');
+        Route::post('update','update');
+        Route::delete('delete/{id}','delete');
+        Route::get('trashshow','trashshow');
+        Route::post('restore/{id}','restore');
+    });
+
+    Route::prefix('divisions')->controller(DivisionController::class)->group(function() {
+        Route::get('show','show');
+        Route::post('create','create');
+        Route::post('update','update');
+        Route::post('delete','delete');
+        Route::post('restore','restore');
+    });
+
+    Route::prefix('cities')->controller(CityController::class)->group(function() {
+        Route::post('show','show');
+        Route::post('create','create');
+        Route::post('update','update');
+        Route::post('delete','delete');
+        Route::post('restore','restore');
+    });
+
+    Route::prefix('townships')->controller(TownshipController::class)->group(function() {
+        Route::post('show','show');
+        Route::post('create','create');
+        Route::post('update','update');
+        Route::post('delete','delete');
+        Route::post('restore','restore');
+    });
+
+    Route::prefix('shops')->controller(ShopController::class)->group(function() {
+        Route::post('show','show');
+        Route::post('create','create');
+        Route::post('update','update');
+        Route::post('delete','delete');
+        Route::post('restore','restore');
+    });
+
+    Route::prefix('categories')->controller(CategoryController::class)->group(function() {
+        Route::post('show','show');
+        Route::post('create','create');
+        Route::post('update','update');
+        Route::post('delete','delete');
+        Route::post('restore','restore');
+    });
+
+    Route::prefix('items')->controller(ItemController::class)->group(function() {
+        Route::post('show','show');
+        Route::post('create','create');
+        Route::post('update','update');
+        Route::post('delete','delete');
+        Route::post('restore','restore');
+    });
+});
+
 
 
 
