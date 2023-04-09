5aba0cbc484c6a4dc959679777e7fbbdb0185280
Thein Thein Soe <theintheinsoe1218@gmail.com>
2023-04-09 10:16:08 +0630

    list category edited

--- resources/js/components/Admin/Category/ListCategories.jsx
+++ resources/js/components/Admin/Category/ListCategories.jsx
@@ -12,165 +12,156 @@ import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
 
 const ListCategories = () => {
-    const {axios,setCategory,categories,setCategories} = useAdminContext();
-    const [show, setShow] = useState(false);
-    const [shops,setShops] = useState([]);
-    const [filterText, setFilterText] = useState('');
+const {axios,setCategory,categories,setCategories} = useAdminContext();
+const [show, setShow] = useState(false);
+const [shops,setShops] = useState([]);
+const [filterText, setFilterText] = useState('');
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+const handleClose = () => setShow(false);
+const handleShow = () => setShow(true);
 
-    const getCategories = ()=>{
-        axios.post(`/api/admin/categories/showAll`).then((res)=>{
-            setCategories(res.data);
-        })
-    }
+const getCategories = ()=>{
+axios.post(`/api/admin/categories/showAll`).then((res)=>{
+setCategories(res.data);
+})
+}
 
-    useEffect(()=>{
-        getCategories();
-        
-    },[])
+useEffect(()=>{
+getCategories();
 
-    // console.log(categories);
-    const [showedit, setEditShow] = useState(false);
-    const editClose = () => setEditShow(false);
+},[])
 
-    const editShow = (e,row) => {
-        e.preventDefault();
-        setEditShow(true);
-        setCategory(row);
-    }
+// console.log(categories);
+const [showedit, setEditShow] = useState(false);
+const editClose = () => setEditShow(false);
 
-    const deleteCategory = (e,id)=>{
-        const data = {
-            id:id
-        }
-        axios.post(`/api/admin/categories/delete`,data).then((res)=>{
-            // console.log(res);
-            toast.success(res.data.message, {
-                position: "top-right",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: false,
-                pauseOnHover: false,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-                });
-            getCategories();
-        })
-    }
-    const columns = [
-        {
-            name: 'ID',
-            selector: row => row.id,
-            width:"70px",
-            sortable: true,
-    
-        },
-        {
-            name: 'Shop',
-            selector: row => row.shop.shop_name,
-            wrap:true,
-            sortable: true,
-    
-        },
-        {
-            name: 'Name',
-            selector: row => row.name,
-            sortable: true,
-        },
-        {
-            name: 'Remark',
-            selector: row => row.remark,
-            wrap:true,
-        },
-        {
-            name: 'Action',
-            selector: (row) =>
-            <div>
-                <button
-                    className='btn btns me-2'
-                    onClick={(e)=>editShow(e,row)}
-                >Edit
-                </button>
-                
-            </div>,
-            width:'200px'
-            
-            
-        },
-        {   
-            selector: (row) =>
-            <button
-                    className='btn btn-danger'
-                    onClick={(e)=>deleteCategory(e,row.id)}
-                >Delete
-            </button>   
-        }
- 
+const editShow = (e,row) => {
+e.preventDefault();
+setEditShow(true);
+setCategory(row);
+}
 
-        
-        
-    ];
 
-    return (
-        <>
-            {/* <div className="container">
-                <button className='btn mb-2 btns' onClick={handleShow}>Add Category</button>
-                <Link to="/admin/categories/detetedrecord" className="btn float-end mb-2 btns">Trashed Record</Link>
-            </div> */}
-            <ToastContainer/>
-            <DataTable
-                title="Category Lists"
-                columns={columns}
-                data={categories}
-                fixedHeader
-                fixedHeaderScrollHeight="300px"
-                pagination
-                responsive
-                highlightOnHover
-            />
-            
-            {/* ADD CATEGORY */}
-            <Modal size="lg" show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Add Category</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <AddCategory handleClose={handleClose}/>
-                </Modal.Body>
-                <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
-                    Cancel
-                </Button>
-                <Button className='btns' type="submit" form="addcategory">
-                    Save
-                </Button>
-                </Modal.Footer>
-            </Modal>
 
-            {/* EDIT CATEGORY */}
 
-            <Modal size="lg" show={showedit} onHide={editClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Edit Category</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <EditCategory handleClose={editClose}/>
-                </Modal.Body>
-                <Modal.Footer>
-                <Button variant="secondary" onClick={editClose}>
-                    Cancel
-                </Button>
-                <Button className='btns' type="submit" form="updatecategory">
-                    Update
-                </Button>
-                </Modal.Footer>
-            </Modal>
-   
-        </>
-    )
+const deleteCategory = (e,id)=>{
+const data = {
+id:id
+}
+axios.post(`/api/admin/categories/delete`,data).then((res)=>{
+// console.log(res);
+toast.success(res.data.message, {
+position: "top-right",
+autoClose: 3000,
+hideProgressBar: false,
+closeOnClick: false,
+pauseOnHover: false,
+draggable: true,
+progress: undefined,
+theme: "light",
+});
+getCategories();
+})
+}
+const columns = [
+{
+name: 'ID',
+selector: row => row.id,
+width:"70px",
+sortable: true,
+
+},
+{
+name: 'Shop',
+selector: row => row.shop.shop_name,
+wrap:true,
+sortable: true,
+
+},
+{
+name: 'Name',
+selector: row => row.name,
+sortable: true,
+},
+{
+name: 'Remark',
+selector: row => row.remark,
+wrap:true,
+},
+{
+name: 'Action',
+selector: (row) =>
+<div>
+    <button className='btn btns me-2' onClick={(e)=>editShow(e,row)}
+        >Edit
+    </button>
+
+</div>,
+width:'200px'
+
+
+},
+{
+selector: (row) =>
+<button className='btn btn-danger' onClick={(e)=>deleteCategory(e,row.id)}
+    >Delete
+</button>
+}
+
+
+
+
+];
+
+return (
+<>
+    {/* <div className="container">
+        <button className='btn mb-2 btns' onClick={handleShow}>Add Category</button>
+        <Link to="/admin/categories/detetedrecord" className="btn float-end mb-2 btns">Trashed Record</Link>
+    </div> */}
+    <ToastContainer />
+    <DataTable title="Category Lists" columns={columns} data={categories} fixedHeader fixedHeaderScrollHeight="300px"
+        pagination responsive highlightOnHover />
+
+    {/* ADD CATEGORY */}
+    <Modal size="lg" show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+            <Modal.Title>Add Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <AddCategory handleClose={handleClose} />
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                Cancel
+            </Button>
+            <Button className='btns' type="submit" form="addcategory">
+                Save
+            </Button>
+        </Modal.Footer>
+    </Modal>
+
+    {/* EDIT CATEGORY */}
+
+    <Modal size="lg" show={showedit} onHide={editClose}>
+        <Modal.Header closeButton>
+            <Modal.Title>Edit Category</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <EditCategory handleClose={editClose} />
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={editClose}>
+                Cancel
+            </Button>
+            <Button className='btns' type="submit" form="updatecategory">
+                Update
+            </Button>
+        </Modal.Footer>
+    </Modal>
+
+</>
+)
 }
 
 export default ListCategories;
